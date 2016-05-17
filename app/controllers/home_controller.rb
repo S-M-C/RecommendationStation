1147f65f916372d29c9a1d763dcb2c00a005dc43
malcolm-mergulhao <mmerg_1995@hotmail.com>
2016-05-17 18:21:38 -0500

    added modal window for displaying recommendations

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,16 @@
 class HomeController < ShopifyApp::AuthenticatedController
+  
   def index
     @products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
   end
+  
+  def rec_modal
+    @products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
+  end
+  
+  def get_number_of_matches(pid)
+    5
+  end
+  
+  helper_method :get_number_of_matches
 end
