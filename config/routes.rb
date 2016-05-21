346abc54c44f73ce97296adabbd66c061d26acf9
malcolm-mergulhao <mmerg_1995@hotmail.com>
2016-05-21 16:50:14 -0500

    base for searching

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   mount ShopifyApp::Engine, at: '/'
   
   get 'rec_modal' => "home#rec_modal", :as => :rec_modal
-
+  get 'search' => "home#search", :as => :search
+  get 'index' => "home#index", :as => :index
+  
   root :to => 'home#index'
  
 end
