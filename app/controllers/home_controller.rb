81143a4af7da1d310866eaf470438e6a460bc307
CoxT9 <CoxT3@myumanitoba.ca>
2016-06-03 19:41:43 -0500

    type filtering works

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,22 +5,31 @@ class HomeController < ShopifyApp::AuthenticatedController
     if not keyword.nil? and not keyword.empty?
       keyword.strip!
     end
-    #params[:keyword] = "hello"
     collection = params[:collection]
     type = params[:type]
 
-
     # add each non empty field to filter calls
-    @collections = ShopifyAPI::CustomCollection.find(:all,:params => {:limit => 10})
-    @types = ShopifyAPI::Product.find(:all, :params => {:limit => 10 }).map{|product| [product.product_type,0]}.uniq.delete_if{|type| type[0].nil? || type[0].empty?}
+    @collections = ShopifyAPI::CustomCollection.find(:all,:params => {:limit => 10}).map{|collection| [collection.title]}.uniq.delete_if{|type| type[0].nil? || type[0].empty?}
+    @types = ["Any"]+ShopifyAPI::Product.find(:all, :params => {:limit => 10 }).map{|product| [product.product_type]}.uniq.delete_if{|type| type[0].nil? || type[0].empty?}
 
-    
+    products_type = nil
+    if type != "Any"
+      products_type = ShopifyAPI::Product.find(:all, :params => {:limit => 10, :product_type => type } )
+    end
+
+    products_coll = nil # what about products that do not belong to a collection
+    if collection != "Any"
+      current_collection = ShopifyAPI::CustomCollection.find(:all, :params => {:limit => 10, :title => collection })[0]
+      products_coll = current_collection.products
+      for p in products_coll
+        print p
+      end
+    end
+
 
     products_kwrd = ShopifyAPI::Product.find(:all, :params => {:limit => 10, :title => keyword } )
-    products_coll = ShopifyAPI::Product.find(:all, :params => {:limit => 10, :collection => collection } )
-    products_type = ShopifyAPI::Product.find(:all, :params => {:limit => 10, :type => type } ) 
 
-    @products = intersect(products_kwrd, products_coll, products_type)
+    @products = intersect(products_kwrd, products_coll, products_type) # find a better way to filter empty lists
     # filtering is easy. think of approach for multi attribute filters
     
   end
